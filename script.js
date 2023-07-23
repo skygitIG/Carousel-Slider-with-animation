9c04820316609b6ddba3cce7d905a05f218a26ea
Akash Kumar <117715724+skygitIG@users.noreply.github.com>
2023-07-23 16:47:06 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,21 @@
+// by
+// @sky.git
+// skygit.netlify.app
+
+// And the journey to CSS only components continues...
+
+/*
+  In this one I'm using CSS selectors and little hack/tricks to archive proper sliding effect between slides, the old one would be something like this when you change the slide:
+  the 1st one goes backward(left) and the 2nd one comes forward(center) and if you change it to 3rd...
+  the 2nd one goes backward(left) and the 3rd one comes forward(center) and so on...
+*/
+
+/*
+  It makes sliding weird and most of the times we've to use JavaScript to archive proper sliding, so I start experimenting and is able to archive proper sliding effect with CSS only :)
+*/
+
+/*
+  Images are from Unsplash 
+*/
+
+// Thanks for viewing
